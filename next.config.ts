6dd7c95012cc37c93659da08fb9ced2bcff79faf
Ekaterina V. <hitomizavr@gmail.com>
2025-09-26 17:46:53 +0300

    fix(config): an experimental function is enabled in the config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    globalNotFound: true,
+  },
   images: {
     deviceSizes: [
       360, // Very small mobile
